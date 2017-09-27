aaa41b305e1fd9ba07d198e4b926d942443bd966
leandromoh <leandromoh@hotmail.com>
2017-09-27 19:16:00 -0300

    working

--- SmartEnum.Test/SmartEnumHelper.cs
+++ SmartEnum.Test/SmartEnumHelper.cs
@@ -10,25 +10,18 @@ using System;
     [SetUpFixture]
     public class SmartEnumHelper
     {
-        public SmartEnumHelper()
-        {
-
-        }
-
-        public static string GetDescriptionFromDescriptionTestAttribute(FieldInfo field)
-        {
-            var customAttributes = field
-                                       .GetCustomAttributes(typeof(DescriptionTestAttribute), false)
-                                       .Cast<DescriptionTestAttribute>()
-                                       .ToList();
-
-            return customAttributes.FirstOrDefault()?.Description ?? "teste";
-        }
-
         [OneTimeSetUp]
         public void RunBeforeAnyTests()
         {
-            EnumExtensions.GetDescriptionFromField = SmartEnumHelper.GetDescriptionFromDescriptionTestAttribute;
+            EnumExtensions.GetDescriptionFromField = (FieldInfo field) =>
+            {
+                var customAttributes = field
+                                        .GetCustomAttributes(typeof(DescriptionTestAttribute), false)
+                                        .Cast<DescriptionTestAttribute>()
+                                        .ToList();
+
+                return customAttributes.FirstOrDefault()?.Description ?? "teste";
+            };
         }
     }
 //}
